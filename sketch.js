4d7d0754af098ac3a64dc6fad0875e121b1ad65a
FlowerSeerat <seerat3208@gmail.com>
2021-08-11 08:34:11 -0400

    Background & score

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg,bgImg;
+var score=0;
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +46,27 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(bgImg){
+        background(bgImg);
+    }
+    noStroke();
+    textSize(35);
+    fill("white");
+    text("Score: " +score, width-300, 50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();  
+    
     log3.display();
 
     box5.display();
@@ -66,7 +76,9 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();  
+  
+   
 }
 
 function mouseDragged(){
@@ -85,4 +97,20 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+async function getBackgroundImg() {
+    var response = await fetch("http://worldclockapi.com/api/json/est/now");
+    var responseJSON = await response.json();
+  //  console.log(responseJSON.currentDateTime);
+    var datetime=responseJSON.currentDateTime;
+    var hour=datetime.slice(11,13);
+    //console.log(hour);
+    if (hour>=06&&hour<=19){
+     bg="sprites/bg.png"
+    }else{
+        bg="sprites/bg2.jpg"
+    }
+    bgImg=loadImage(bg);
+    console.log(bgImg);
 }
